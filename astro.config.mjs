121a1a14cdb63fc27016c4fac3623b58928543df
mahereenafser <mahereen.afser08@gmail.com>
2025-08-30 03:47:22 +0600

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,9 +3,4 @@ import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
   integrations: [tailwind()],
-  site: 'https://yoursite.vercel.app',
-  build: {
-    output: 'dist',  // Specify the output directory
-    clean: true,     // Clean output directory before building
-  },
 });
