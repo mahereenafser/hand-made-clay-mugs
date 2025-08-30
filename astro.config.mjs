4a37732fe7a9ad0b71377b8f5a2f65887e485e43
mahereenafser <mahereen.afser08@gmail.com>
2025-08-30 12:24:37 +0600

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,5 +3,8 @@ import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
   integrations: [tailwind()],
-  site: 'https://hand-made-clay-mugs-9rrjpzrw9-mahereens-projects.vercel.app/',  // Replace this with your actual site URL once deployed
+  site: 'https://hand-made-clay-mugs-9rrjpzrw9-mahereens-projects.vercel.app/',
+  build: {
+    output: 'server',
+  },// Replace this with your actual site URL once deployed
 });
